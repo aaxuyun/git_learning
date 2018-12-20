a0528a19ef873e62453d05121c5824c34f7182b3
aaxuyun <784266498@qq.com>
2018-12-20 14:54:35 +0800

    update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 export default class App extends Component {
     render() {
         return (
-            <div>Hello React!</div>
-            <div>first line</div>
             <div>first line2</div>
         );
     }
